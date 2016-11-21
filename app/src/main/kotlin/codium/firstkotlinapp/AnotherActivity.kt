c7e2dbbe89e6be5f1c76f476ed80e8f14446404d
Chakpiwat Pacharajindawat <chakpiwat.p@codium.co>
2016-11-21 17:59:13 +0700

    create form with anko using mock feel text.xml

---
+++ app/src/main/kotlin/codium/firstkotlinapp/AnotherActivity.kt
@@ -0,0 +1,7 @@
+package codium.firstkotlinapp
+
+/**
+ * Created by game on 2111//59 พ.ศ..
+ */
+class AnotherActivity {
+}
--- app/src/main/kotlin/codium/firstkotlinapp/MainActivity.kt
+++ app/src/main/kotlin/codium/firstkotlinapp/MainActivity.kt
@@ -2,17 +2,44 @@ package codium.firstkotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.RelativeLayout
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        button.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
+        relativeLayout {
+            editText {
+                id = Ids.editText
+            }.lparams(width = wrapContent, height = wrapContent) {
+                alignParentLeft()
+                alignParentTop()
+                alignParentRight()
+                alignParentStart()
+                alignParentEnd()
+            }
+            button("New Button") {
+                id = Ids.button
+            }.lparams(width = wrapContent, height = wrapContent) {
+                below(Ids.editText)
+                alignParentRight()
+                alignParentEnd()
+            }
+            editText {
+                id = Ids.editText2
+            }.lparams(width = wrapContent, height = wrapContent) {
+                sameTop(Ids.button)
+                alignParentLeft()
+                alignParentStart()
+            }
         }
     }
+
+    private object Ids {
+        val button = 1
+        val editText = 2
+        val editText2 = 3
+    }
+
 }
