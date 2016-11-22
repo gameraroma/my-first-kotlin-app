04b3f00bd9c95844b2dd96888b22dc868592d665
Chakpiwat Pacharajindawat <chakpiwat.p@codium.co>
2016-11-22 17:45:59 +0700

    preview image after take a picture

--- app/src/main/kotlin/codium/firstkotlinapp/CameraActivity.kt
+++ app/src/main/kotlin/codium/firstkotlinapp/CameraActivity.kt
@@ -1,16 +1,19 @@
 package codium.firstkotlinapp
 
+import android.app.Activity
 import android.os.Bundle
 import org.jetbrains.anko.*
 import android.content.Intent
 import android.graphics.Paint
 import android.provider.MediaStore.ACTION_IMAGE_CAPTURE
 import android.provider.MediaStore
-
+import android.graphics.Bitmap
+import android.widget.ImageView
 
 
 class CameraActivity : MainActivity(){
     val REQUEST_IMAGE_CAPTURE = 1
+    var preview_image: ImageView? = null
 
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -32,7 +35,13 @@ class CameraActivity : MainActivity(){
             }.lparams(width = wrapContent, height = wrapContent) {
                 alignParentBottom()
             }
+            preview_image = imageView {}.lparams(width = wrapContent, height = wrapContent) {
+                above(id=2)
+                centerHorizontally()
+                alignParentEnd()
+            }
             button("Take Picture") {
+                id = CameraActivity.Ids.button_take_picture
                 onClick {
                     dispatchTakePictureIntent()
                 }
@@ -45,5 +54,15 @@ class CameraActivity : MainActivity(){
 
     private object Ids {
         val button_finish = 1
+        val button_take_picture = 2
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val extras = data.extras
+            val imageBitmap = extras.get("data") as Bitmap
+            preview_image!!.setImageBitmap(imageBitmap)
+        }
     }
 }
